2cf228bf61c26f6559a88ab30137b66e22d10157
ACCLARISCORP\Debmallya.Bhattachar <debmallya.bhattacharya@acclaris.com>
2019-05-29 11:40:37 +0530

    removed unused import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from './services/product.service';
 import { Product } from './interfaces/product';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
